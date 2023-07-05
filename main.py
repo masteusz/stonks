a4c0df33932d2d0c9a32165a637869d43d29e37b
Mateusz Stankiewicz <mateusz@mateusz.at>
2023-07-05 16:25:14 +0200

    feat: add basic window

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import structlog
+
+from gui.mainwindow import MainWindow
 from utils import configure_logging
 
+logger = structlog.stdlib.get_logger()
 if __name__ == "__main__":
     configure_logging()
+    logger.info("Starting app")
+    gui = MainWindow()
--- utils.py
+++ utils.py
@@ -1,2 +1,58 @@
-def configure_logging():
-    pass
+import logging
+from enum import Enum
+
+import structlog
+from structlog.typing import Processor
+
+_shared_processors: list[Processor] = [
+    structlog.contextvars.merge_contextvars,
+    structlog.processors.add_log_level,
+    structlog.stdlib.PositionalArgumentsFormatter(),
+    structlog.stdlib.ExtraAdder(),
+    structlog.processors.StackInfoRenderer(),
+    structlog.dev.set_exc_info,
+    structlog.processors.TimeStamper(fmt="iso"),
+]
+
+
+class LogLevel(str, Enum):
+    CRITICAL = "CRITICAL"
+    FATAL = "FATAL"
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+    WARN = "WARN"
+    INFO = "INFO"
+    DEBUG = "DEBUG"
+    NOTSET = "NOTSET"
+
+
+def configure_logging() -> None:
+    # based on this junk: https://gist.github.com/nymous/f138c7f06062b7c43c060bf03759c29e
+
+    log_level = logging.getLevelName(LogLevel.DEBUG)
+    coloring_enabled = True
+
+    structlog.configure(
+        processors=_shared_processors + [structlog.dev.ConsoleRenderer(colors=coloring_enabled)],
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=False,
+    )
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        # These run ONLY on `logging` entries that do NOT originate within structlog.
+        foreign_pre_chain=_shared_processors,
+        # These run on ALL entries after the pre_chain is done.
+        processors=[
+            # Remove _record & _from_structlog.
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            structlog.dev.ConsoleRenderer(colors=coloring_enabled),
+        ],
+    )
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
+    root_logger.setLevel(log_level)
